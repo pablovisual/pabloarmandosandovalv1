0f6039428ebc93d478a4523ffcadeb7575f89467
Pablo Sandoval <pablo.a.sandoval01@utrgv.edu>
2021-02-24 15:50:51 -0600

    added attrs to target className from styled componenets

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ function Header() {
           <p>experience</p>
           <p>projects</p>
           <p>education</p>
-          <p>contact</p>
+          <p className="last">contact</p>
         </HeaderRight> 
       </HeaderContainer>
     )
@@ -25,11 +25,14 @@ export default Header
 
 const HeaderContainer = styled.div `
   display: flex;
+  position: fixed;
   align-items: center;
   border-bottom: 1px solid lightgray;
   padding: 10px;
-  background-color: darkgray;
-
+  background: none;
+  width: 100vw;
+  top: 0;
+  z-index: 999;
 `;
 
 const HeaderLeft = styled.div`
@@ -37,15 +40,22 @@ const HeaderLeft = styled.div`
 `;
 const HeaderAvatar = styled(Avatar)``;
 
-const HeaderRight = styled.div`
+const HeaderRight = styled.div.attrs(props => ({
+  className: props.className,
+}))`
   display: flex;
   > p {
-    margin: 10px;
+    margin: 15px;
+    justify-content: center;
     text-transform: uppercase;
   }
   > p:hover {
       cursor: pointer;
-      color: white;
+      color: lightgray;
+  }
+
+  & .last {
+    padding-right: 15px;
   }
   
 `
